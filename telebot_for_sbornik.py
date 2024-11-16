c50b6c5909b71197d373d22e01c7796fc0cfa108
mcventik pro <mcventik@yandex.ru>
2024-11-16 22:35:35 +0400

    Конечный вариант рабочего примера, который будет взят за основу

--- telebot_for_sbornik.py
+++ telebot_for_sbornik.py
@@ -1,5 +1,5 @@
 import asyncio
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
 # Функция для обработки команды /start
@@ -7,23 +7,23 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
         [InlineKeyboardButton("Официант", callback_data='oficiant'), InlineKeyboardButton("Хостес", callback_data='hostes')],
         [InlineKeyboardButton("Повар", callback_data='povar'), InlineKeyboardButton("Бармен", callback_data='barmen')],
-        #[InlineKeyboardButton("Назад", callback_data='back')]
+
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     # Путь к локальному файлу
     photo_path = "foto.jfif"
-    
+    caption="Привет, рады видеть в команде ресторана 'Перк'. Наш адрес г. Саратов, ул. Вольская, д. 63/69. Телефон для связи +79878250490. Телефон для обратной связи о стажировке. Выбери должность и ознакомься с памяткой"
+
     # Отправка фото
     await context.bot.send_photo(
         chat_id=update.effective_chat.id,
         photo=open(photo_path, 'rb'),
-        caption="Привет, рады видеть в команде ресторана 'Перк'. Наш адрес г. Саратов, ул. Вольская, д. 63/69. Телефон для связи +79878250490. Телефон для обратной связи о стажировке. Выбери должность и ознакомься с памяткой",
-        #reply_markup = reply_markup
-     )
-
-    await update.message.reply_text('Привет, рады видеть в команде ресторана "Перк". Наш адрес г. Саратов, ул. Вольская, д. 63/69. Телефон для связи +79878250490. Телефон для обратной связи о стажировке +79649987878 - Ольга. Выбери должность и ознакомься с памяткой', reply_markup=reply_markup)
+        caption=caption,
+        reply_markup=reply_markup
+    )
+    
 
 # Функция для обработки нажатий кнопок
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -32,35 +32,44 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     # Ответ на выбранный вариант
     if query.data == 'oficiant':
-        text = "Вы выбрали должность: Официант. Нажмите 'Назад', чтобы вернуться в меню."
+        photo_path = "oficiant.jpg"  # Фото для "Официант"
+        caption = "Вы выбрали должность: Официант. Нажмите 'Назад', чтобы вернуться."
         back_keyboard = [[InlineKeyboardButton("Назад", callback_data="назад")]]
         reply_markup = InlineKeyboardMarkup(back_keyboard)
-        await query.edit_message_text(text=text, reply_markup=reply_markup)
     elif query.data == 'hostes':
-        text = "Вы выбрали должность: Хостес. Нажмите 'Назад', чтобы вернуться в меню."
+        photo_path = "hostes.jpg"  # Фото для "Официант"
+        caption = "Вы выбрали должность: Хостес. Нажмите 'Назад', чтобы вернуться."
         back_keyboard = [[InlineKeyboardButton("Назад", callback_data="назад")]]
         reply_markup = InlineKeyboardMarkup(back_keyboard)
-        await query.edit_message_text(text=text, reply_markup=reply_markup)
     elif query.data == 'povar':
-        text = "Вы выбрали должность: Повар. Нажмите 'Назад', чтобы вернуться в меню."
+        photo_path = "povar.jpg"  # Фото для "Официант"
+        caption = "Вы выбрали должность: Повар. Нажмите 'Назад', чтобы вернуться."
         back_keyboard = [[InlineKeyboardButton("Назад", callback_data="назад")]]
         reply_markup = InlineKeyboardMarkup(back_keyboard)
-        await query.edit_message_text(text=text, reply_markup=reply_markup)
     elif query.data == 'barmen':
-        text = "Вы выбрали должность: Бармен. Нажмите 'Назад', чтобы вернуться в меню."
+        photo_path = "barmen.jpg"  # Фото для "Официант"
+        caption = "Вы выбрали должность: Бармен. Нажмите 'Назад', чтобы вернуться."
         back_keyboard = [[InlineKeyboardButton("Назад", callback_data="назад")]]
         reply_markup = InlineKeyboardMarkup(back_keyboard)
-        await query.edit_message_text(text=text, reply_markup=reply_markup)
     elif query.data == "назад":
         # Возвращаем меню выбора кнопок
-        caption="Привет, рады видеть в команде ресторана 'Перк'. Наш адрес г. Саратов, ул. Вольская, д. 63/69. Телефон для связи +79878250490. Телефон для обратной связи о стажировке. Выбери должность и ознакомься с памяткой",
+        photo_path = "foto.jfif"
+        caption="Привет, рады видеть в команде ресторана 'Перк'. Наш адрес г. Саратов, ул. Вольская, д. 63/69. Телефон для связи +79878250490. Телефон для обратной связи о стажировке. Выбери должность и ознакомься с памяткой"
         keyboard = [
                 [InlineKeyboardButton("Официант", callback_data='oficiant'), InlineKeyboardButton("Хостес", callback_data='hostes')],
                 [InlineKeyboardButton("Повар", callback_data='povar'), InlineKeyboardButton("Бармен", callback_data='barmen')],
                 #[InlineKeyboardButton("Назад", callback_data='back')]
             ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_caption(caption=caption, reply_markup=reply_markup) 
+        await query.edit_message_media(
+        media=InputMediaPhoto(open(photo_path, 'rb'), caption=caption),
+        reply_markup=reply_markup)
+        return
+    #Для функций кроме ф-ции "назад"
+    await query.edit_message_media(
+        media=InputMediaPhoto(open(photo_path, 'rb'), caption=caption),
+        reply_markup=reply_markup
+    )
 
 # Основная функция
 async def main():
